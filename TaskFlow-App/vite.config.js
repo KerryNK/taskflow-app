48bc1e403c2bd681daed647db342c68259d2c4b6
Ciarrai <163757149+KerryNK@users.noreply.github.com>
2025-06-26 00:52:06 +0300

    Update vite.config.js

--- TaskFlow-App/vite.config.js
+++ TaskFlow-App/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+export default defineConfig({
+  base: '/taskflow-app/',
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
